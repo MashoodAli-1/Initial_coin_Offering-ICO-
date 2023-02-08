0528d06241977b7b903f841d63fe828fffd8464b
Mashood Ali <mashoodali219@gmail.com>
2023-02-08 21:54:25 +0500

    ICO
    First commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x47a4c1877F6540cE668CB74c083CB3131a78735f";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
